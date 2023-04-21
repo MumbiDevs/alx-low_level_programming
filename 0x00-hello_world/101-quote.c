026c0c59659068254e846253779c3aab92d4ffde
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-04-21 04:52:07 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,13 +7,12 @@
  */
 int main(void)
 {
-    /* Message to be printed*/
-char message[] = "and that piece of art is useful\" 
-- Dora Korpar, 2015-10-19\n";
+    // Message to be printed
+    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-/* Write the message to standard error (stderr)*/
+    // Write the message to standard error (stderr)
     write(2, message, sizeof(message) - 1);
 
-/* Return 1 as per requirement*/
+    // Return 1 as per requirement
     return (1);
 }

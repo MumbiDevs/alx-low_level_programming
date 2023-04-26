99c293d90cebb302b44bce704098e1f7bb834404
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-04-26 04:44:10 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,11 @@
 #include <stdio.h>
 
 /**
- * _putchar - Custom putchar function to print characters to stdout
- * @c: The character to be printed
- *
- * Return: The printed character on success, -1 on error
- */
-int _putchar(char c)
-{
-	return putchar(c);
-}
-
-/**
  * print_alphabet_x10 - Prints the lowercase alphabet 10 times, followed by a newline
  *
  * Description: This function prints the lowercase alphabet (from 'a' to 'z')
- * ten times, using the _putchar function. It then prints a newline character
- * to create a newline at the end of each set of alphabets.
+ * ten times, using the putchar function from stdio.h. It then prints a newline
+ * character to create a newline at the end of each set of alphabets.
  *
  * Return: void
  */
@@ -29,8 +18,8 @@ void print_alphabet_x10(void)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
-			_putchar(c);
+			putchar(c);
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }

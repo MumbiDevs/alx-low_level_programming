528dbaa72ebb6829064249bcf0e4ddcb361e4128
MumbiDevs <rachealdeveloper@gmail.com>
2023-04-25 19:10:16 -0700

    functions and loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,7 +4,7 @@
  * main - finds and prints the first 98 Fibonacci numbers,
  * starting with 1 and 2
  * followed by a new line
- * Return: ALways 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -41,3 +41,4 @@ int main(void)
 
 	return (0);
 }
+

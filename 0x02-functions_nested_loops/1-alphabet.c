8284380168b6cc1388db41611171ba1a14f885e6
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-04-26 04:28:04 +0300

    Update 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+/**
+ * print_alphabet - Prints the lowercase alphabet followed by a newline
+ *
+ * Description: This function prints the lowercase alphabet (from 'a' to 'z')
+ * using the _putchar function. It then prints a newline character to create a
+ * newline at the end of the alphabet.
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c; /* Variable to store each character of the alphabet */
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}

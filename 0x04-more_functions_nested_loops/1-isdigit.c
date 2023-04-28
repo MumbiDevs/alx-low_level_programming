395c439a3000b2db5fbfbd4624fa620b68915dc2
MumbiDevs <rachealdeveloper@gmail.com>
2023-04-27 19:50:29 -0700

    is digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,7 +5,22 @@
  * @c: int to be checked
  * Return: 1 if c is a digit, 0 otherwise
  */
-int _isdigit(int c)
-{
-	return (c >= '0' && c <= '9');
+int _isdigit(int c) {
+    if (c >= '0' && c <= '9') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int main() {
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);
+    if (_isdigit(c)) {
+        printf("The character '%c' is a digit.\n", c);
+    } else {
+        printf("The character '%c' is not a digit.\n", c);
+    }
+    return 0;
 }

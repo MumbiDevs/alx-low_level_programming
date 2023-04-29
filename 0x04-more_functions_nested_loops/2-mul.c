2d3789a179f73a36b354379c45ae212428c2a1cb
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-04-29 04:24:14 +0300

    Update 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int mul(int a, int b);
+
+int main() {
+    int a = 5;
+    int b = 10;
+    int result = mul(a, b);
+    printf("%d * %d = %d\n", a, b, result);
+    return 0;
+}

e025e81dd005e7d38acf0cb0e55ae7bf72980526
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-04-29 04:16:48 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,24 +1,11 @@
-#include <stdio.h>
 #include "main.h"
 
-int _isdigit(int c);
-
-int main() {
-    char c;
-    printf("Enter a character: ");
-    scanf("%c", &c);
-    if (_isdigit(c)) {
-        printf("%c is a digit\n", c);
-    } else {
-        printf("%c is not a digit\n", c);
-    }
-    return 0;
-}
-
-int _isdigit(int c) {
-    if (c >= '0' && c <= '9') {
-        return 1;
-    } else {
-        return 0;
-    }
+/**
+*
+*
+*
+*/
+int _isdigit(int c)
+{
+    return (c >= '0' && c <= '9');
 }

9ffb1640cf4a6c7d93ded92103bb3cbbb926f91c
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-05-04 04:46:15 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,41 +3,26 @@
 #include <time.h>
 #include <string.h>
 
-#define PASSWORD_LENGTH 15
+#define PASSWORD_LENGTH 10
 
-/**
- * main - Generates a random password and checks if it matches the expected value
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char password[PASSWORD_LENGTH + 1]; // +1 for null terminator
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789! ";
+int main(void) {
+    char password[PASSWORD_LENGTH + 1]; /* +1 for null terminator */
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     const int charset_length = sizeof(charset) - 1;
 
-    // Seed the random number generator with the current time
+    /* Seed the random number generator with the current time */
     srand(time(NULL));
 
-    // Generate a random character for each position in the password
-    for (int i = 0; i < PASSWORD_LENGTH; i++)
-    {
+    /* Generate a random character for each position in the password */
+    int i;
+    for (i = 0; i < PASSWORD_LENGTH; i++) {
         password[i] = charset[rand() % charset_length];
     }
 
-    // Add the null terminator to the end of the password
+    /* Add the null terminator to the end of the password */
     password[PASSWORD_LENGTH] = '\0';
 
-    // Check if the generated password matches the expected value
-    if (strcmp(password, "Tada! Congrats") == 0)
-    {
-        printf("Generated password: %s\n", password);
-        printf("Congratulations!\n");
-    }
-    else
-    {
-        fprintf(stderr, "Wrong password\n");
-    }
+    printf("Generated password: %s\n", password);
 
-    return (0);
-} 
+    return 0;
+}

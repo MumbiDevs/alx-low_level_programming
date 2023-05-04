4c03d74691901d6b2e00ea4876395a6da3f93d72
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-05-04 04:35:00 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -20,10 +20,11 @@ int main(void)
     srand(time(NULL));
 
     /* Generate a random character for each position in the password */
-int i;
-for (i = 0; i < PASSWORD_LENGTH; i++) {
-    password[i] = charset[rand() % charset_length];
-}
+    for (int i = 0; i < PASSWORD_LENGTH; i++)
+    {
+        password[i] = charset[rand() % charset_length];
+    }
+
     /* Add the null terminator to the end of the password */
     password[PASSWORD_LENGTH] = '\0';
 

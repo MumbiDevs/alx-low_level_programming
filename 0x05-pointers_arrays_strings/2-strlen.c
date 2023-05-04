af4be13cc67e61da529faa1df16eeb58004cd385
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-05-04 03:20:38 +0300

    Update 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+    int len = 0;
+
+    while (*s != '\0')
+    {
+        len++;
+        s++;
+    }
+
+    return (len);
+}
+
+/**
+ * main - entry point of the program
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+    char str[] = "Hello, world!";
+    int len = _strlen(str);
+
+    printf("Length of string \"%s\": %d\n", str, len);
+
+    return (0);
+}

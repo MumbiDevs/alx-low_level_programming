55f2b6163ba010d943ca58832fd3825350a5bab0
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-05-04 04:51:14 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,43 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
 
-#define PASSWORD_LENGTH 15
+#define PASSWORD_LENGTH 10
 
 /**
- * main - Generates a random password and checks if it matches the expected value
+ * main - Generate a random password for the program 101-crackme
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    char password[PASSWORD_LENGTH + 1]; // +1 for null terminator
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789! ";
-    const int charset_length = sizeof(charset) - 1;
+	char password[PASSWORD_LENGTH + 1]; /* +1 for null terminator */
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	const int charset_length = sizeof(charset) - 1;
+	int i;
 
-    // Seed the random number generator with the current time
-    srand(time(NULL));
+	/* Seed the random number generator with the current time */
+	srand(time(NULL));
 
-    // Generate a random character for each position in the password
-    for (int i = 0; i < PASSWORD_LENGTH; i++)
-    {
-        password[i] = charset[rand() % charset_length];
-    }
+	/* Generate a random character for each position in the password */
+	for (i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		password[i] = charset[rand() % charset_length];
+	}
 
-    // Add the null terminator to the end of the password
-    password[PASSWORD_LENGTH] = '\0';
+	/* Add the null terminator to the end of the password */
+	password[PASSWORD_LENGTH] = '\0';
 
-    // Check if the generated password matches the expected value
-    if (strcmp(password, "Tada! Congrats") == 0)
-    {
-        printf("Generated password: %s\n", password);
-        printf("Congratulations!\n");
-    }
-    else
-    {
-        fprintf(stderr, "Wrong password\n");
-    }
+	printf("Generated password: %s\n", password);
 
-    return (0);
+	return (0);
 }

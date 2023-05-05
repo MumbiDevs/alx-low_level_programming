7ba427e8f115ccb8a56f6a75e696e3fc6c5aa707
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-05-05 15:07:08 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,17 +3,13 @@
 /**
  * print_most_numbers - Prints the numbers from 0 to 9, except for 2 and 4.
  */
-void print_most_numbers(void)
+void print_numbers(void)
 {
     int i;
 
-    for (i = 0; i < 10; i++)
+    for (i = 0; i <= 9; i++)
     {
-        if (i != 2 && i != 4)
-        {
-            _putchar(i + '0');
-        }
+        _putchar(i + '0');
     }
-
     _putchar('\n');
 }

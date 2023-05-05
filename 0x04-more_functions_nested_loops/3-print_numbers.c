dd3315804d29159eb92d2a94a8cc00c7b7272734
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-05-05 15:10:53 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,11 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Prints the numbers from 0 to 9, except for 2 and 4.
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Description: This function prints the numbers 0 through 9 followed by a new line
+ *
+ * Return: void
  */
 void print_numbers(void)
 {

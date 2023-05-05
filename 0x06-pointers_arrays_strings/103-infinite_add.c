3ed704603467d9ccbeaed37d4f7ef83ac6100749
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-05-05 12:56:14 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,48 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+/**
+ * infinite_add - Adds two numbers
+ * @n1: First number to add
+ * @n2: Second number to add
+ * @r: Buffer to store the result
+ * @size_r: Size of the result buffer
+ *
+ * Return: Pointer to the result buffer, or 0 if the result can't be stored
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
     int len1 = strlen(n1);
     int len2 = strlen(n2);
-    int lenr = size_r - 1; // leave room for the null terminator
     int carry = 0;
-    
-    // iterate over the two input strings from right to left
-    while (len1 > 0 || len2 > 0)
+    int sum = 0;
+    int i, j;
+
+    /* Make sure the result buffer is large enough to hold the sum */
+    if (len1 + len2 > size_r - 1)
     {
-        int d1 = (len1 > 0) ? (n1[--len1] - '0') : 0;
-        int d2 = (len2 > 0) ? (n2[--len2] - '0') : 0;
-        int sum = d1 + d2 + carry;
-        if (sum > 9) {
-            carry = 1;
-            sum -= 10;
-        } else {
-            carry = 0;
-        }
-        if (lenr <= 0) {
-            return 0; // result too large for buffer
-        }
-        r[--lenr] = sum + '0';
+        return (0);
     }
-    
-    // add any leftover carry over at the end
-    if (carry > 0)
+
+    /* Add the numbers digit by digit, starting from the least significant digit */
+    for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry; i--, j--)
     {
-        if (lenr <= 0) {
-            return 0; // result too large for buffer
-        }
-        r[--lenr] = carry + '0';
+        int digit1 = i >= 0 ? n1[i] - '0' : 0;
+        int digit2 = j >= 0 ? n2[j] - '0' : 0;
+        sum = digit1 + digit2 + carry;
+        carry = sum / 10;
+        r[i + j + 1] = (sum % 10) + '0';
     }
-    
-    // pad the result with leading zeroes if necessary
-    while (lenr > 0 && lenr < size_r - 1)
+
+    /* Add the null terminator to the result buffer */
+    r[len1 + len2] = '\0';
+
+    /* Reverse the order of the digits in the result buffer */
+    for (i = 0, j = len1 + len2 - 1; i < j; i++, j--)
     {
-        r[--lenr] = '0';
+        char temp = r[i];
+        r[i] = r[j];
+        r[j] = temp;
     }
-    
-    // add null terminator and return pointer to result
-    r[size_r - 1] = '\0';
-    return &r[lenr];
+
+    return (r);
 }

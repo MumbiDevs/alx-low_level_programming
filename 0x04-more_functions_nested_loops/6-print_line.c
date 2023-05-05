61b9764cbe535eca01c5f39008f62725721ee048
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-05-05 15:26:34 +0300

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_line - Draws a straight line in the terminal
- * @n: Number of times the character '_' should be printed
+ * print_line - draws a straight line in the terminal
+ * @n: the number of times the character _ should be printed
  *
  * Return: void
  */
@@ -14,7 +14,9 @@ void print_line(int n)
         return;
     }
 
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
+    {
         _putchar('_');
+    }
     _putchar('\n');
 }

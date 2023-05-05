68fde78c30496f18006ae85f38bde5e8586c90e7
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-05-05 15:35:35 +0300

    Update 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - prints the numbers from 1 to 100, with FizzBuzz rules
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+            printf("FizzBuzz ");
+        else if (i % 3 == 0)
+            printf("Fizz ");
+        else if (i % 5 == 0)
+            printf("Buzz ");
+        else
+            printf("%d ", i);
+    }
+    printf("\n");
+
+    return (0);
+}

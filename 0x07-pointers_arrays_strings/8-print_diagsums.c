e9506d650b302228de5b0f44e63fcc451a720c56
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-05-08 17:42:14 +0300

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,36 +2,21 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - Print the sum of the two diagonals of a square matrix
- *                  of integers
+ * print_diagsums - Prints the sum of the diagonals of a square matrix of integers
+ * @a: Pointer to the first element of the matrix
+ * @size: Size of the matrix (number of rows and columns)
  *
- * @a: The pointer to the first element of the matrix
- * @size: The size of the matrix (number of rows/columns)
+ * Return: void
  */
 void print_diagsums(int *a, int size)
 {
-    int i, sum1 = 0, sum2 = 0;
-    int *p1 = a, *p2 = a + size - 1;
+	int i, sum1 = 0, sum2 = 0;
 
-    /* Iterate through each element of the matrix */
-    for (i = 0; i < size * size; i++)
-    {
-        /* If the element is on the main diagonal, add it to sum1 */
-        if (i % (size + 1) == 0)
-        {
-            sum1 += *p1;
-            p1 += size + 1;
-        }
+	for (i = 0; i < size; i++)
+	{
+		sum1 += *(a + i * size + i);
+		sum2 += *(a + i * size + (size - i - 1));
+	}
 
-        /* If the element is on the secondary diagonal, add it to sum2 */
-        if (i % (size - 1) == 0 && i != size * (size - 1))
-        {
-            sum2 += *p2;
-            p2 += size - 1;
-        }
-    }
-
-    /* Print the sums */
-    printf("Sum of main diagonal: %d\n", sum1);
-    printf("Sum of secondary diagonal: %d\n", sum2);
+	printf("%d, %d\n", sum1, sum2);
 }

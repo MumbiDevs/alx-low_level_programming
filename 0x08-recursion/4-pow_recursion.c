bf3078f3a9293d80df055806c7e4216e9501d10e
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-05-11 13:39:38 +0300

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * _pow_recursion - returns the value of x raised to the power of y, using recursion
+ * _pow_recursion - returns the value of x
+ * raised to the power of y, using recursion
  *
  * @x: the base number
  * @y: the exponent

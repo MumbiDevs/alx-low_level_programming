d80095bb3b81d54f5166831a836d48d1cbfcf554
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-05-11 13:22:01 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,40 +1,16 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * find_sqrt - finds the natural square root of a number between a given range, using recursion
+ * find_sqrt - finds the natural square root of a number between a given range,
+ *             using recursion
  *
  * @n: the number to find the natural square root of
  * @start: the starting point of the range to search
  * @end: the ending point of the range to search
  *
- * Return: the natural square root of n, or -1 if n does not have a natural square root
+ * Return: the natural square root of n, or -1 if n does not have a natural
+ *         square root
  */
-int find_sqrt(int n, int start, int end);
-
-/**
- * _sqrt_recursion - returns the natural square root of a number, using recursion
- *
- * @n: the number to find the natural square root of
- *
- * Return: the natural square root of n, or -1 if n does not have a natural square root
- */
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-	{
-		return (-1);
-	}
-	else if (n == 0 || n == 1)
-	{
-		return (n);
-	}
-	else
-	{
-		return (find_sqrt(n, 1, n));
-	}
-}
-
 int find_sqrt(int n, int start, int end)
 {
 	int mid = (start + end) / 2;
@@ -57,5 +33,29 @@ int find_sqrt(int n, int start, int end)
 		{
 			return (find_sqrt(n, mid + 1, end));
 		}
+	}
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number, using recursion
+ *
+ * @n: the number to find the natural square root of
+ *
+ * Return: the natural square root of n, or -1 if n does not have a natural
+ *         square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		return (find_sqrt(n, 1, n));
 	}
 }

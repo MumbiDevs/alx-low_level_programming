ca2d5c990ea4fccf3efd3e6e6d9293cc323d952c
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-05-11 13:46:21 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,27 +3,51 @@
 #include <string.h>
 
 /**
- * is_palindrome - checks if a string is a palindrome
+ * _strlen_recursion - returns the length of a string using recursion
+ * @s: the string to count the length of
  *
+ * Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (_strlen_recursion(s + 1) + 1);
+}
+
+/**
+ * is_palindrome_helper - helps to check if a string is a palindrome
+ * @s: the string to check
+ * @start: the index of the start character
+ * @end: the index of the end character
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
+int is_palindrome_helper(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+
+	if (s[start] != s[end])
+		return (0);
+
+	return (is_palindrome_helper(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
  * @s: the string to check
  *
- * Return: 1 if @s is a palindrome, 0 otherwise
+ * Return: 1 if the string is a palindrome, 0 otherwise
  */
 int is_palindrome(char *s)
 {
-    int len = strlen(s);
+	int len;
 
-    if (len <= 1)
-    {
-        return (1);
-    }
-    else if (s[0] == s[len - 1])
-    {
-        s[len - 1] = '\0';
-        return (is_palindrome(s + 1));
-    }
-    else
-    {
-        return (0);
-    }
+	len = _strlen_recursion(s);
+	if (len == 0)
+		return (1);
+
+	return (is_palindrome_helper(s, 0, len - 1));
 }

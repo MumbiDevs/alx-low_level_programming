43664db58fb8caadb8ff381c6f44a0f610e2b037
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-05-23 15:32:31 +0300

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
 
 /**
- * printFileName - Prints the name of the file it was compiled from
- */
-void printFileName(void)
-{
-	printf("Compiled from file: %s\n", __FILE__);
-}
-
-/**
  * main - Entry point of the program
  *
- * Description: Calls the printFileName function to print the file name.
+ * Description: This program prints the name of the file it was compiled from.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printFileName();
-	return 0;
+	printf("Compiled from file: %s\n", __FILE__);
+	return (0);
 }

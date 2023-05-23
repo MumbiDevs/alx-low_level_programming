a2877d113beb2e72c79a0b4098215c57d1a2eb42
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-05-23 15:37:26 +0300

    Create 3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * ABS - Computes the absolute value of a number
+ * @x: The number to compute the absolute value of
+ *
+ * Description: This macro evaluates the absolute value of the given number.
+ *              It returns the absolute value of a signed integer, floating-point,
+ *              or character constant.
+ */
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+
+int main(void)
+{
+	int num = -10;
+	float fnum = -5.5;
+
+	printf("Absolute value of %d: %d\n", num, ABS(num));
+	printf("Absolute value of %.2f: %.2f\n", fnum, ABS(fnum));
+
+	return 0;
+}

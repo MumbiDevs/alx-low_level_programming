e325c60a837a706825b95df986dec60709191fed
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-05-23 15:42:21 +0300

    Create 4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * SUM - Computes the sum of two numbers
+ * @x: The first number
+ * @y: The second number
+ *
+ * Description: This macro evaluates the sum of the given numbers.
+ *              It returns the result of adding the two numbers together.
+ */
+#define SUM(x, y) ((x) + (y))
+
+int main(void)
+{
+	int a = 5;
+	int b = 7;
+
+	printf("Sum of %d and %d: %d\n", a, b, SUM(a, b));
+
+	return 0;
+}

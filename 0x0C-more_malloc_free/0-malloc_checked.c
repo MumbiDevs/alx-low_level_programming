06c0dc50a70c58d05cd8c8cbeeba8079165ae418
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-05-27 16:59:48 +0300

    Create 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: The number of bytes to allocate
+ *
+ * Return: A pointer to the allocated memory
+ *         Terminate process with status 98 on failure
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		fprintf(stderr, "malloc failed\n");
+		exit(98);
+	}
+
+	return (ptr);
+}
+
+int main(void)
+{
+	unsigned int size = 100;
+	int *arr = malloc_checked(size * sizeof(int));
+
+	/* Use the allocated memory */
+
+	free(arr);
+	return (0);
+}

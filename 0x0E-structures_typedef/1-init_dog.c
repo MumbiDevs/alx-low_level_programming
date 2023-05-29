fd08eebc01b92cc4ec153d546228096019eafb1d
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-05-29 17:24:43 +0300

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,11 +9,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 {
-	init_dog(&my_dog, "Buddy", 3.5, "John");
-
-	printf("Name: %s\n", my_dog.name);
-	printf("Age: %.1f\n", my_dog.age);
-	printf("Owner: %s\n", my_dog.owner);
-
-	return 0;
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }

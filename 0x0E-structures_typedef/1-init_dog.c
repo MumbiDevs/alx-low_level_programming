1786217db059aa9fffbcc88aec900bee33c545b2
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-05-29 14:04:29 +0300

    Create 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * struct dog - Represents information about a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * init_dog - Initializes a struct dog.
+ * @d: Pointer to the struct dog to be initialized.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: None.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+
+int main(void)
+{
+	struct dog my_dog;
+
+	init_dog(&my_dog, "Buddy", 3.5, "John");
+
+	printf("Name: %s\n", my_dog.name);
+	printf("Age: %.1f\n", my_dog.age);
+	printf("Owner: %s\n", my_dog.owner);
+
+	return 0;
+}

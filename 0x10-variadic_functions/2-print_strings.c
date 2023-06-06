89cb7bbd022a14bbf1d3083146d0913f0fcf9355
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-06-06 09:28:04 +0300

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdio.h>
 
 /**
  * print_strings - Prints strings followed by a new line
@@ -12,16 +13,23 @@ void print_strings(const char *separator, const unsigned int n, ...)
     va_list args;
     va_start(args, n);
 
-    for (unsigned int i = 0; i < n; ++i) {
+    for (unsigned int i = 0; i < n; ++i)
+    {
         char *str = va_arg(args, char *);
 
         if (str == NULL)
+        {
             printf("(nil)");
+        }
         else
+        {
             printf("%s", str);
+        }
 
         if (separator != NULL && i != n - 1)
+        {
             printf("%s", separator);
+        }
     }
 
     va_end(args);

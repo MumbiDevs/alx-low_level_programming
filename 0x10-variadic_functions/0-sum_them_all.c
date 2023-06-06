a87d848862818f0150415ec052c4bb289f5f3684
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-06-06 08:00:29 +0300

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,18 +11,16 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
-		return (0);
-
+	unsigned int i;
 	int sum = 0;
+	va_list list;
 
-	va_list args;
-	va_start(args, n);
+	va_start(list, n);
 
-	for (unsigned int i = 0; i < n; ++i)
-		sum += va_arg(args, int);
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
 
-	va_end(args);
+	va_end(list);
 
 	return (sum);
 }

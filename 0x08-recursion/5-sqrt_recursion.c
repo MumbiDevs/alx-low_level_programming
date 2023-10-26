0796bce460559bbdf838841f25c306733324c3c1
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-10-26 16:00:19 +0300

    Update 5-sqrt_recursion.c
    -

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,28 +1,6 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Calculates the natural square root of a number recursively.
- * @n: The number for which to calculate the square root.
- *
- * Return: The natural square root of 'n'. If n does not have a natural
- * square root, return -1.
- */
-int _sqrt_recursion(int n)
-{
-    if (n < 0)
-    {
-        return -1; /* Error: n is negative, no natural square root. */
-    }
-
-    if (n == 0 || n == 1)
-    {
-        return n; /* Base case: the square root of 0 or 1 is itself. */
-    }
-
-    return find_sqrt(n, 1); /* Call helper function to find square root. */
-}
-
-/**
  * find_sqrt - Helper function to find the square root recursively.
  * @n: The number for which to calculate the square root.
  * @guess: The current guess for the square root.
@@ -43,4 +21,26 @@ int find_sqrt(int n, int guess)
     {
         return find_sqrt(n, guess + 1); /* Continue searching. */
     }
+}
+
+/**
+ * _sqrt_recursion - Calculates the natural square root of a number recursively.
+ * @n: The number for which to calculate the square root.
+ *
+ * Return: The natural square root of 'n'. If n does not have a natural
+ * square root, return -1.
+ */
+int _sqrt_recursion(int n)
+{
+    if (n < 0)
+    {
+        return -1; /* Error: n is negative, no natural square root. */
+    }
+
+    if (n == 0 || n == 1)
+    {
+        return n; /* Base case: the square root of 0 or 1 is itself. */
+    }
+
+    return find_sqrt(n, 1); /* Call helper function to find square root. */
 }

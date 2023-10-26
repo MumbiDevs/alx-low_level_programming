cbe331dd73b24e4f204091f6b744030509cc59b4
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-10-26 12:16:29 +0300

    Update 0-puts_recursion.c
    main duplicate adjusted

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 
-/**
- * _puts_recursion - Prints a string, followed by a new line.
- * @s: The string to be printed.
- */
 void _puts_recursion(char *s)
 {
     if (*s == '\0')
@@ -15,11 +11,4 @@ void _puts_recursion(char *s)
 
     putchar(*s); /* Print the current character */
     _puts_recursion(s + 1); /* Recursively call the function with the next character */
-}
-
-int main()
-{
-    char *str = "Hello, World!";
-    _puts_recursion(str);
-    return (0);
 }

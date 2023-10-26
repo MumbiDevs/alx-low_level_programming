26b8101639920f0125ee0ef567f8fdf9ba234aa1
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-10-26 12:53:18 +0300

    Update 1-print_rev_recursion.c
    -

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,19 +7,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0')
+    if (*s)
     {
-        return; /* Base case: Do nothing when the end of the string is reached. */
+        _print_rev_recursion(s + 1);
+        putchar(*s);
     }
-
-    _print_rev_recursion(s + 1); /* Recursively call the function with the next character. */
-    putchar(*s); /* Print the current character in reverse order. */
-}
-
-int main(void)
-{
-    char *str = "Hello, World!";
-    _print_rev_recursion(str);
-    putchar('\n'); /* Print a new line after reversing the string. */
-    return (0);
 }

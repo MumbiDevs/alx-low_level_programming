1dd421b8d1073649fcc11a5cc6f235088a39060f
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-10-26 15:45:42 +0300

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,11 +5,12 @@ int factorial(int n)
 {
     if (n < 0)
     {
-        return -1; // Error: n is less than 0.
+        return -1; /* Error: n is less than 0. */
     }
     int result = 1;
+    int i;
 
-    for (int i = 2; i <= n; i++)
+    for (i = 2; i <= n; i++)
     {
         result *= i;
     }

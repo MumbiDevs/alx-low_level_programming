de51a5b625ace1a0790f881403629e56d388e16e
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-10-26 15:47:15 +0300

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * factorial - Calculates the factorial of a given number.
+ * @n: The number for which to calculate the factorial.
+ *
+ * Return: The factorial of 'n'. If 'n' is less than 0, return -1.
+ */
 int factorial(int n)
 {
+    int result = 1; // Declare variables at the beginning of the block.
+    int i;
+
     if (n < 0)
     {
         return -1; /* Error: n is less than 0. */
     }
-    int result = 1;
-    int i;
 
     for (i = 2; i <= n; i++)
     {

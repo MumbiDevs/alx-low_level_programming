a851ca0070b41a1eb5133f510fa92e6cd1ede207
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-10-26 12:18:29 +0300

    Update 0-puts_recursion.c
    Betty style implemented

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * _puts_recursion - Prints a string, followed by a new line recursively.
+ * @s: The string to be printed.
+ */
 void _puts_recursion(char *s)
 {
     if (*s == '\0')

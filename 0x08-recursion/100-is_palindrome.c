63709fd83884b07d30e3ae93995c6491a409e150
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-10-27 11:46:58 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,41 +1,34 @@
 #include <stdio.h>
-#include "main.h"
+#include "stdio.h"
 
 /**
- * is_palindrome - Check if a string is a palindrome using recursion.
+ * is_palindrome - Check if a string is a palindrome.
  * @s: The input string.
  *
  * Return: 1 if the string is a palindrome, 0 otherwise.
  */
 int is_palindrome(char *s) {
-    if (s == NULL || *s == '\0') {
-        return 1; // An empty string is a palindrome.
+    int len, i, j;
+
+    if (s == NULL) {
+        return 0; /* Handle the case of a NULL string. */
     }
 
-    int len = 0;
+    len = 0;
     while (s[len] != '\0') {
         len++;
     }
 
-    return check_palindrome(s, 0, len - 1);
-}
+    i = 0;
+    j = len - 1;
 
-/**
- * check_palindrome - Helper function to check if a substring of a string is a palindrome recursively.
- * @s: The input string.
- * @start: The starting index of the substring.
- * @end: The ending index of the substring.
- *
- * Return: 1 if the substring is a palindrome, 0 otherwise.
- */
-int check_palindrome(char *s, int start, int end) {
-    if (start >= end) {
-        return 1; // Base case: It's a palindrome.
+    while (i < j) {
+        if (s[i] != s[j]) {
+            return 0; /* The string is not a palindrome. */
+        }
+        i++;
+        j--;
     }
 
-    if (s[start] != s[end]) {
-        return 0; // Characters at the current positions don't match.
-    }
-
-    return check_palindrome(s, start + 1, end - 1);
+    return 1; /* The string is a palindrome. */
 }

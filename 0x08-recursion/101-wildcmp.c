02bf17cf6c8cbbf285119a0f732f541d159d1053
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-10-27 12:50:14 +0300

    Update 101-wildcmp.c
    -

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,21 +9,18 @@
  * Return: 1 if the strings can be considered identical, 0 otherwise.
  */
 int wildcmp(char *s1, char *s2) {
-    if (*s2 == '*') {
-        if (*(s2 + 1) == '*') {
-            return wildcmp(s1, s2 + 1); /* Skip consecutive '*'.*/
-        }
-        while (*s1 != '\0') {
-            if (wildcmp(s1, s2 + 1)) {
-                return 1; /* Matched the remaining of s2 with s1. */
-            }
-            s1++;
-        }
-        return wildcmp(s1, s2 + 1); /* Skip '*' in s2. */
+    if (*s2 == '\0') {
+        return (*s1 == '\0'); /* If s2 is empty, s1 must also be empty for a match. */
     }
 
-    if (*s1 == '\0' && *s2 == '\0') {
-        return 1; /* Both strings are empty, considered identical. */
+    if (*s2 == '*') {
+        if (wildcmp(s1, s2 + 1)) {
+            return 1; /+ '*' matches an empty string in s1. */
+        }
+        if (*s1 != '\0' && wildcmp(s1 + 1, s2)) {
+            return 1; /* '*' matches a character in s1.*/
+        }
+        return 0; /* '*' doesn't match, so s1 and s2 are not identical. */
     }
 
     if (*s1 == *s2) {

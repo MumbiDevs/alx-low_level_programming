19309abbbf03cf449a3894811ef7d9590d668a35
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2023-10-27 11:12:08 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,28 +7,29 @@
  *
  * Return: 1 if the string is a palindrome, 0 otherwise.
  */
+int is_palindrome_recursive(char *s, int start, int end);
 int is_palindrome(char *s) {
-    int len, i, j;
-
     if (s == NULL) {
-        return 0; /* Handle the case of a NULL string. */
+        return 0; // Handle the case of a NULL string.
     }
 
-    len = 0;
-    while (s[len] != '\0') {
-        len++;
+    int len = strlen(s);
+
+    if (len <= 1) {
+        return 1; // A string with 0 or 1 character is a palindrome.
     }
 
-    i = 0;
-    j = len - 1;
+    return is_palindrome_recursive(s, 0, len - 1);
+}
 
-    while (i < j) {
-        if (s[i] != s[j]) {
-            return 0; /* The string is not a palindrome. */
-        }
-        i++;
-        j--;
+int is_palindrome_recursive(char *s, int start, int end) {
+    if (start >= end) {
+        return 1; // Base case: It's a palindrome.
     }
 
-    return 1; /* The string is a palindrome. */
+    if (s[start] != s[end]) {
+        return 0; // Characters at the current positions don't match.
+    }
+
+    return is_palindrome_recursive(s, start + 1, end - 1);
 }

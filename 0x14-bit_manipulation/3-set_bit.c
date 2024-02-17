5e94798facc3862873afe0ce4c476340e43f6e44
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-02-17 13:27:39 +0300

    Create 3-set_bit.c
    Write a function that sets the value of a bit to 1 at a given index.

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: pointer to the decimal number
+ * @index: index position to change, starting from 0
+ *
+ * Return: 1 if it worked, -1 if error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index > sizeof(unsigned long int) * 8 - 1)
+		return (-1); /* Index out of bounds */
+
+	mask = 1UL << index; /* Create a mask with the bit at index set to 1 */
+
+	*n |= mask; /* Set the bit at index to 1 */
+
+	return (1);
+}
